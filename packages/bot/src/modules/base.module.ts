afb2981f30cdf0ecd674c7d598f1c7836f2d54a1
lanvalird <strazhognay@gmail.com>
2026-01-19 00:28:54 +0400

    bot/modules: add abstract prototype for BaseModule

---
+++ packages/bot/src/modules/base.module.ts
@@ -0,0 +1,35 @@
+import { Bot } from '../client/index.js';
+
+export interface IModule {
+  readonly name: string;
+  readonly dependencies?: string[];
+
+  initialize(bot: Bot): Promise<void>;
+  destroy(): Promise<void>;
+}
+
+/** @todo WIP */
+interface IEvent {
+  name: string;
+  once?: boolean;
+  execute: (...args: any[]) => void;
+}
+
+export abstract class BaseModule implements IModule {
+  public abstract readonly name: string
+  public readonly dependencies: string[] = [];
+  protected bot!: Bot;
+  protected events: IEvent[] = [];
+
+  public async initialize(bot: Bot): Promise<void> {
+    this.bot = bot;
+    await this.setup();
+    await this.registerEvents()
+  }
+
+  abstract setup(): Promise<void>;
+
+  public async registerEvents(): Promise<void> {}
+
+  public async destroy(): Promise<void> {}
+}
